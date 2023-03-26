ae02d65828a1c207e29949346378da4d603375c9
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-26 09:28:18 +0530

    ♻️  update macros

--- 4-cmsis/src/main.c
+++ 4-cmsis/src/main.c
@@ -1,48 +1,31 @@
-#include <stdint.h>
-#include "stm32f401xe.h"
 #include "stm32f4xx.h"
+#include <stdint.h>
 
-enum { GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG };
-
-/* Function declaration */
-extern void _estack(void);
-
-static inline void gpio_set(GPIO_TypeDef *port, uint32_t pin);
-static inline void gpio_reset(GPIO_TypeDef * port, uint32_t pin);
+static inline void gpio_toggle(GPIO_TypeDef *port, uint32_t pin);
 
 void ms_delay(int ms);
 
+int main(void) {
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 
-int main(void)
-{
-	RCC->AHB1ENR |= 1 << 2;
+    GPIOC->MODER &= ~GPIO_MODER_MODER13;
 
-	GPIOC->MODER &= ~ 3U  << (13 * 2);
+    GPIOC->MODER |= GPIO_MODER_MODER13_0; // Set OUTPUT mode
 
-    GPIOC->MODER |= (GPIO_MODE_OUTPUT & 3U) << (13 * 2);   // Set new mode
-
-	while(1){
-        gpio_set(GPIOC, 13);
-		ms_delay(500);
-        gpio_reset(GPIOC, 13);
-		ms_delay(500);
-	}
+    while (1) {
+        gpio_toggle(GPIOC, GPIO_ODR_OD13);
+        ms_delay(500);
+    }
 }
 
-void ms_delay(int ms)
-{
-    while (ms-- > 0)
-    {
+void ms_delay(int ms) {
+    while (ms-- > 0) {
         volatile int x = 1000;
         while (x-- > 0)
             __asm("nop");
     }
 }
 
-static inline void gpio_set(GPIO_TypeDef * port, uint32_t pin){
-    port->BSRR |= 1 << pin;
-}
-
-static inline void gpio_reset(GPIO_TypeDef * port, uint32_t pin){
-    port->BSRR |= 1 << (pin + 16);
+static inline void gpio_toggle(GPIO_TypeDef *port, uint32_t pin) {
+    port->ODR ^= pin;
 }
