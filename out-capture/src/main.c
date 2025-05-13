d09119b6c51464cf3dbe052e0aa494be68212ebf
Rohit Nimkar <nehalnimkar@gmail.com>
2025-05-13 16:15:37 +0530

    📝 update comments

--- out-capture/src/main.c
+++ out-capture/src/main.c
@@ -1,10 +1,11 @@
 /**
  * @file        main.c
- * @description Scan for connected I2C devices and log device address
+ * @brief       Timer3 configuration with 4 output compare channels on STM32F4
+ * @details     Demonstrates configuration of TIM3 with 4 output compare channels and toggles an LED using a timer interrupt.
  * @author      Rohit Nimkar
  * @version     1.0
- * @date        2024-07-07
- * @copyright   Copyright 2024 Rohit Nimkar
+ * @date        2025-05-12
+ * @copyright   Copyright 2025 Rohit Nimkar
  *
  * @attention
  *  Use of this source code is governed by a BSD-style
@@ -15,9 +16,26 @@
 #include "stm32f4xx.h"
 #include "gpio.h"
 
+/**
+ * @brief   Timer interrupt flag. Set in TIM3_IRQHandler, cleared in delay_ms().
+ */
 volatile int flag = 0;
 
-void Timer1_Init(void)
+/**
+ * @brief   Initializes Timer 3 (TIM3) with 4 output compare channels.
+ * @details
+ *  - Enables clocks for GPIOA, GPIOB, and TIM3.
+ *  - Configures PA6, PA7, PB0, PB1 as alternate function (AF2) for TIM3 CH1-CH4.
+ *  - Sets prescaler and auto-reload for 1ms period.
+ *  - Configures output compare for all 4 channels.
+ *  - Enables TIM3 interrupt in NVIC.
+ *  - Starts the timer.
+ *
+ * @note
+ *  - Output compare values set for demonstration (200, 400, 600, 800).
+ *  - GPIO configuration assumes default push-pull output.
+ */
+void Timer3_Init(void)
 {
     /* Enable Clock for GPIOA and GPIOB */
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN;
@@ -44,88 +62,104 @@ void Timer1_Init(void)
     GPIOB->AFR[0] &= ~(GPIO_AFRL_AFRL0 | GPIO_AFRL_AFRL1);
     GPIOB->AFR[0] |= GPIO_AFRL_AFRL0_1 | GPIO_AFRL_AFRL1_1;
 
-    /*---- Enable clock for TIM3 ----*/
+    /* Enable clock for TIM3 */
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 
-    /*----
-        Set prescaler value
-        Timer clock = SystemCoreClock / (prescaler + 1)
-        Assuming SystemCoreClock is 16 MHz, for 1 ms => 16 - 1 = 15
-    ----*/
-    TIM3->PSC = 15999; // Prescaler to get 1 MHz (1 µs tick)
+    /* Timer clock frequency = SystemCoreClock / (PSC + 1)
+     * Assuming SystemCoreClock is 16 MHz,
+     * To get a timer clock of 1 KHz (1 ms per tick): (16 MHz / 1 KHz) - 1 = 15999
+     */
+    TIM3->PSC = 15999;
 
-    // Set auto-reload register (ARR) for 1 ms
-    TIM3->ARR = 500 - 1; // 1 ms
+    /* For a 1s timer period with 1ms ticks: 1s / 1ms = 1000 counts
+     * Since counting starts from 0, ARR should be set to 999.
+     */
+    TIM3->ARR = 1000 - 1;
 
     /* Enable update interrupt */
     TIM3->DIER |= TIM_DIER_UIE;
 
-    // Enable NVIC for TIM1 update interrupt
+    /* Enable NVIC for TIM3 update interrupt */
     NVIC_EnableIRQ(TIM3_IRQn);
 
-    /* Write compare values */
-    TIM3->CCR1 = 100 - 1;
-    TIM3->CCR2 = 200 - 1;
-    TIM3->CCR3 = 300 - 1;
-    TIM3->CCR4 = 400 - 1;
+    /* Set output compare values for all 4 channels */
+    TIM3->CCR1 = 200 - 1;
+    TIM3->CCR2 = 400 - 1;
+    TIM3->CCR3 = 600 - 1;
+    TIM3->CCR4 = 800 - 1;
 
-    /* Toggle CH1 and CH2 on Match */
+    /* Configure output compare toggle mode for CH1 and CH2 */
     TIM3->CCMR1 &= ~(TIM_CCMR1_OC1M | TIM_CCMR1_OC2M);
-    TIM3->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_0; 
+    TIM3->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_0;
 
-    /* Enable TIM3 CHannel 1, 2, 3, 4 */
-    TIM3->CCMR1 |= TIM_CCMR1_OC2PE| TIM_CCMR1_OC1PE;
+    /* Enable preload for CH1 and CH2, set as output */
+    TIM3->CCMR1 |= TIM_CCMR1_OC2PE | TIM_CCMR1_OC1PE;
     TIM3->CCMR1 &= ~(TIM_CCMR1_CC1S | TIM_CCMR1_CC2S);
 
-
+    /* Configure output compare toggle mode for CH3 and CH4 */
     TIM3->CCMR2 &= ~(TIM_CCMR2_OC3M | TIM_CCMR2_OC4M);
-    TIM3->CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_0 | TIM_CCMR2_OC4M_1 | TIM_CCMR2_OC4M_0; 
+    TIM3->CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_0 | TIM_CCMR2_OC4M_1 | TIM_CCMR2_OC4M_0;
 
+    /* Enable preload for CH3 and CH4, set as output */
     TIM3->CCMR2 |= TIM_CCMR2_OC3PE | TIM_CCMR2_OC4PE;
     TIM3->CCMR2 &= ~(TIM_CCMR2_CC3S | TIM_CCMR2_CC4S);
 
+    /* Enable all 4 channels */
     TIM3->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E | TIM_CCER_CC4E;
 
     /* Enable timer */
     TIM3->CR1 |= TIM_CR1_CEN;
 }
 
+/**
+ * @brief   TIM3 interrupt handler.
+ * @details Clears the update interrupt flag and sets the global flag.
+ */
 void TIM3_IRQHandler(void)
 {
-    // Check if update interrupt flag is set
+    /* Check if update interrupt flag is set */
     if (TIM3->SR & TIM_SR_UIF)
     {
-        TIM3->SR &= ~TIM_SR_UIF; // Clear the interrupt flag
+        TIM3->SR &= ~TIM_SR_UIF; /* Clear the interrupt flag */
         flag = 1;
     }
 }
 
+/**
+ * @brief   Millisecond delay using TIM3 update interrupt.
+ * @param   ms [in]
+ *          Number of milliseconds to delay.
+ * 
+ * @details Waits for the timer interrupt flag for each ms.
+ */
 void delay_ms(uint32_t ms)
 {
-    // for (uint32_t i = 0; i < ms; i++)
-    {
-        // Wait for the update interrupt
-        while (!flag)
-            ;
-        flag = 0;
-    }
+    /* Wait for the update interrupt */
+    while (!flag)
+        __WFI();
+    flag = 0;
 }
 
+/**
+ * @brief   Main program entry point.
+ * @details Configures PC13 as output, initializes TIM3, and toggles PC13 every 500 ms.
+ * @return  int Always returns 0.
+ */
 int main(void)
 {
+    /* Enable clock for GPIOC */
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 
+    /* Set PC13 as output */
     GPIOC->MODER &= ~GPIO_MODER_MODER13;
-
     GPIOC->MODER |= GPIO_MODER_MODER13_0;
 
-    Timer1_Init();
-
-    // SysTick_Config(16000);
+    /* Initialize Timer3 */
+    Timer3_Init();
 
     while (1)
     {
         GPIOToggle(GPIOC, GPIO_ODR_OD13);
-        delay_ms(500);
+        delay_ms(1000);
     }
 }
