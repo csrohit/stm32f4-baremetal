d0576e7b4a24e713504af64a283020750e90d0f0
Rohit Nimkar <nehalnimkar@gmail.com>
2025-10-26 14:43:49 +0530

    usart working

--- cmake-projects/usart/src/main.cpp
+++ cmake-projects/usart/src/main.cpp
@@ -16,6 +16,42 @@
 #include <stdint.h>
 
 
+void usart_init(uint32_t baud)
+{
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+    GPIOA->MODER &= ~ (GPIO_MODER_MODER9 | GPIO_MODER_MODER10);
+    GPIOA->MODER |= GPIO_MODER_MODE9_1 | GPIO_MODER_MODER10_1;
+    GPIOA->OSPEEDR = 0U;
+
+    /* Set alternate functions: USART1 -> AF7 */
+    GPIOA->AFR[1] &= ~(GPIO_AFRH_AFSEL9 | GPIO_AFRH_AFSEL10);
+    GPIOA->AFR[1] |= GPIO_AFRH_AFSEL9_0 | GPIO_AFRH_AFSEL9_1 | GPIO_AFRH_AFSEL9_2 | GPIO_AFRH_AFSEL10_0 | GPIO_AFRH_AFSEL10_1 | GPIO_AFRH_AFSEL10_2;
+
+    USART1->BRR = 16000000U/baud;
+    USART1->CR1 |= USART_CR1_TE | USART_CR1_UE | USART_CR1_RE;
+}
+
+void usart_txchar(char c)
+{
+    // wait for tx data register to be empty
+    while (!(USART1->SR & USART_SR_TXE))
+        ;
+    USART1->DR = 0x000000ff & c;
+}
+
+void usart_txbuf(const char *ch)
+{
+    while (*ch)
+    {
+        usart_txchar(*ch);
+        ch++;
+    }
+
+    // __disable_irq();
+}
+
 
 volatile uint32_t msTicks = 0U;
 
@@ -45,6 +81,7 @@ void ms_delay(uint32_t ms)
 static inline void gpio_toggle(GPIO_TypeDef *port, uint32_t pin);
 
 
+const char* str = "hello\r\n";
 int main(void) {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 
@@ -52,11 +89,13 @@ int main(void) {
 
     GPIOC->MODER |= GPIO_MODER_MODER13_0; // Set OUTPUT mode
     
+    usart_init(115200U);
     SysTick_Config(16000);
 
     while (1) {
         gpio_toggle(GPIOC, GPIO_ODR_OD13);
         ms_delay(1000);
+        usart_txbuf(str);
     }
 }
 
